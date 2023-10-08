a79f378d5d6e99005bcef7f515229dd787c2078e
Mohammad-AbuJaber <mohammadabujaber2001@gmail.com>
2023-10-08 18:05:50 +0300

    Adding Searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,30 @@
+import React, {useState} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+
 const SearchBar = () => {
-    return ("");
+    const [isFocused, setIsFocused] = useState(false);
+
+    const handleFocus = () => {
+        setIsFocused(true);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+    };
+
+    return (
+        <div className={`search-bar ${isFocused ? 'focused' : ''}`}>
+            <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+            <input
+                type="text"
+                placeholder="Find members"
+                className="search-input"
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
+        </div>
+    );
 };
 
 export default SearchBar;
