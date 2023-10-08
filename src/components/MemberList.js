d826495c05d7e7743269240502d17d6a5d65f6d6
Mohammad-AbuJaber <mohammadabujaber2001@gmail.com>
2023-10-08 18:06:21 +0300

    Adding MemberList component

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,5 +1,42 @@
+import React, {useState} from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+
 const MemberList = () => {
-    return ("");
+    const [isChecked, setIsChecked] = useState([]);
+    const members = ['Member 1', 'Member 2', 'Member 3', 'Member 4'];
+
+    const handleCheckboxChange = (index) => {
+        const updatedChecked = [...isChecked];
+        updatedChecked[index] = !updatedChecked[index];
+        setIsChecked(updatedChecked);
+    };
+
+    return (
+        <div className="member-list">
+            {members.map((member, index) => (
+                <div key={index} className="member-item">
+                    <div className="member-info">
+                        <FontAwesomeIcon
+                            icon={faUser}
+                            className="user-icon"
+                            style={{color: isChecked[index] ? 'black' : 'gray'}}
+                        />
+                        <span className="member-name">{member}</span>
+                    </div>
+                    <div className="member-checkbox">
+                        <input
+                            type="checkbox"
+                            id={`checkbox-${index}`}
+                            checked={isChecked[index]}
+                            onChange={() => handleCheckboxChange(index)}
+                        />
+                        <label htmlFor={`checkbox-${index}`}></label>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default MemberList;
